5ce0be5e66d96b50304ffe0d8f0bbaeaa560d361
GeoWing <divinitus@hanmail.net>
2016-03-31 14:34:03 -0700

    Update Features.js

--- layers/Features.js
+++ layers/Features.js
@@ -1,8 +1,12 @@
 var geojson_Features = {
 "type": "FeatureCollection",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-                                                                                
 "features": [
+
+{ "type": "Feature", "properties": { "SHAPE_Leng": 35.764130, "Name": "Impervious_concrete", "LEFT_FID": 0, "RIGHT_FID": 0 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -122.17269181398525, 37.478400715033125, 17.561600000000908 ], [ -122.17276576767544, 37.47832213784239, 17.5228000000061 ] ] ] } },
+{ "type": "Feature", "properties": { "SHAPE_Leng": 25.770194, "Name": "Fence", "LEFT_FID": 0, "RIGHT_FID": 0 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -122.17262504534852, 37.477644163736997, 19.981599999999162 ], [ -122.17256858359521, 37.477698794440428, 19.494300000005751 ] ] ] } },
 
 ]
 }
+
+
